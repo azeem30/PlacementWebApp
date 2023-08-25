7bc169bfa534da15f15c729501246cb43d8bd4f7
Azeem Pinjari <pinjariashpak3@gmail.com>
2023-08-25 21:23:57 +0530

    little changes in front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Background from './Components/Background';
 import Questions from './Components/Questions';
 
 function App() {
   return (
     <div>
+      <Background />
       <Questions />
     </div>
   );
