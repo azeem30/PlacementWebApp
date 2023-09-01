1ccff98c457f96e8a195e80d67b18258e5909870
Azeem Pinjari <pinjariashpak3@gmail.com>
2023-09-01 21:15:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'; 
 import Navbar from './Components/Navbar';
 import Sthome from './Components/studentComponents/Sthome';
+import Schtest from './Components/teacherComponents/Schtest';
 import Stlogin from './Components/studentComponents/Stlogin';
 import Tclogin from './Components/teacherComponents/Tclogin';
 import TcHome from './Components/teacherComponents/TcHome';
