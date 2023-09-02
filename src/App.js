b45d3716b708b8eb67224504ace94ff6bd73c038
OmMahajan29 <ommahajan6@gmail.com>
2023-09-02 23:43:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Background from './Components/Background';
-import Questions from './Components/Questions';
+import './App.css'; 
+import Navbar from './Components/Navbar';
+import Sthome from './Components/studentComponents/Sthome';
+import Schtest from './Components/teacherComponents/Schtest';
+import Stlogin from './Components/studentComponents/Stlogin';
+import Tclogin from './Components/teacherComponents/Tclogin';
+import TcHome from './Components/teacherComponents/TcHome';
+import Stsign from './Components/studentComponents/Stsign';
 
 function App() {
+  //Aptify is a temporary title
   return (
-    <div>
-      <Background />
-      <Questions />
-    </div>
+    <>
+      <Navbar title='Aptify'/>
+      <Stsign/>
+    </>
   );
 }
 
