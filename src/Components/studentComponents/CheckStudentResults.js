4255b1c4bf30330491e0639d4d0b2e9bba17926b
Azeem Pinjari <pinjariashpak3@gmail.com>
2023-10-27 21:39:55 +0530

    Made them Dynamic
    Improved the styling and fetching the subjects from the database for the results page.

--- src/Components/studentComponents/CheckStudentResults.js
+++ src/Components/studentComponents/CheckStudentResults.js
@@ -3,10 +3,12 @@ import Layout from '../commonComponents/Layout'
 import { getStudentDetails } from './Stlogin';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '../commonComponents/Navbar';
 
 export default function CheckStudentResults() {
     useEffect(() => { getTestResults(); }, []);
     const [testResults, setTestResults] = useState([]);
+    const [subjects, setSubjects] = useState([]);
     const navigate = useNavigate();
     let containerStyle = {
         position: 'relative',
@@ -30,7 +32,7 @@ export default function CheckStudentResults() {
         try {
             const studentDetails = getStudentDetails();
             const si = studentDetails.studentInfo;
-            const response = await axios.post('http://localhost:9999/get_test_results', { si });
+            const response = await axios.post('http://localhost:9999/get_student_test_results', { si });
             if (response.status === 200) {
                 console.log(response.data.testResults)
                 setTestResults(response.data.testResults);
@@ -47,28 +49,57 @@ export default function CheckStudentResults() {
                 console.error('Error:', error.message);
             }
         }
+        try{
+            const studentDetails = getStudentDetails();
+            const si = studentDetails.studentInfo;
+            const response = await axios.post('http://localhost:9999/get_student_subjects', { si });
+            if (response.status === 200) {
+                console.log(response.data.studentSubjectsList);
+                setSubjects(response.data.studentSubjectsList);
+            } else {
+                console.log('Failed to get data');
+            }
+        }
+        catch(error){
+            if (error.response) {
+                console.error('Server responded with status:', error.response.status);
+                console.error('Response data:', error.response.data);
+            } else if (error.request) {
+                console.error('No response received:', error.request);
+            } else {
+                console.error('Error:', error.message);
+            }
+        }
     }
 
     return (
         <Layout>
-            <div style={containerStyle} className="container rounded w-50 bg-white">
+            <Navbar title='AptiPro' isLoggedIn={true} componentName='StudentResults' />
+            <div style={containerStyle} className="container rounded bg-white">
                 <div className="d-flex justify-content-around">
                     <h4 style={headerStyle} className="card-title">Submitted Tests</h4>
                     <div className="btn-group my-2" style={buttonStyle}>
-                        <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button type="button" id='subject_display' className="btn mt-1 fw-semibold btn-white text-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             Subjects
                         </button>
                         <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">D.S.A</a></li>
-                            <li><a className="dropdown-item" href="#">D.B.M.S</a></li>
-                            <li><a className="dropdown-item" href="#">Java</a></li>
-                            <li><a className="dropdown-item" href="#">Python</a></li>
-                            <li><a className="dropdown-item" href="#">OS</a></li>
-                            <li><a className="dropdown-item" href="#">Aptitude</a></li>
-                        </ul>
+                                {
+                                    subjects.map((subject, index)=>(
+                                        <li 
+                                            className='dropdown-item'
+                                            key={subject.subject_id}
+                                            onClick={()=>{
+                                                const subjectDisplay = document.getElementById('subject_display');
+                                                subjectDisplay.textContent = subject.subject_name 
+                                            }}>
+                                            {subject.subject_name}
+                                        </li>
+                                    ))
+                                }
+                            </ul>
                     </div>
                     <div className="btn-group my-2" style={buttonStyle}>
-                        <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button type="button" className="btn mt-1 fw-semibold btn-white text-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             Sort by
                         </button>
                         <ul className="dropdown-menu">
@@ -87,7 +118,7 @@ export default function CheckStudentResults() {
                                 <p className='my-2'>Difficulty: <span className='fw-semibold'>{result.difficulty}</span></p>
                                 <p className='my-2'>Subject name: <span className="fw-semibold">{result.subject_name}</span></p>
                                 <p className='my-2'>Marks Obtained: <span className="fw-semibold">{result.marks_scored}</span></p>
-                                <p className='my-2'>Percentage: <span className="fw-semibold">{result.percentage}</span></p>
+                                <p className='my-2'>Percentage: <span className="fw-semibold">{`${parseFloat(result.percentage).toFixed(2)}%`}</span></p>
                             </li>
                         </div>
                     ))}
